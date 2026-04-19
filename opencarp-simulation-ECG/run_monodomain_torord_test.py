e5015640bfe340d607c54a99a0f72c2dd830db40
Federico Omb <155232271+FedeOmb@users.noreply.github.com>
2026-04-19 16:44:23 +0200

    test simulazione carputils monodominio senza torso con torord

--- opencarp-simulation-ECG/run_monodomain_torord_test.py
+++ opencarp-simulation-ECG/run_monodomain_torord_test.py
@@ -4,10 +4,14 @@ import pandas as pd
 from carputils import settings
 from carputils import tools
 
+TORORD_LIB_ENDO = './torord-model-opencarp/ToRORd_fkatp_endo.so'
+TORORD_LIB_MID = './torord-model-opencarp/ToRORd_fkatp_endo_mid.so'
+TORORD_LIB_EPI  = './torord-model-opencarp/ToRORd_fkatp_epi.so'
+
 def parser():
     parser = tools.standard_parser()
     parser.add_argument('--meshname',
-                        default='./sb301_meshes/503kaggle500_opencarp',
+                        default='./sb301_meshes/sb301_fine500um_tagged_opencarp',
                         help='Percorso base della mesh per openCARP')
     parser.add_argument('--vtx-file',
                         default='./sb301_rootnodes/sb301_fine500um_active_root_nodes.vtx',
@@ -16,23 +20,23 @@ def parser():
                         default='./sb301_rootnodes/sb301_candidate_root_nodes_times.csv',
                         help='File CSV contenente i tempi di attivazione (candidate_root_nodes_times.csv)')
     parser.add_argument('--tend',
-                        type=float, default=100.0,
+                        type=float, default=200.0,
                         help='Durata della simulazione in ms')
     parser.add_argument('--outdir',
-                        default='test_monodomain_tentusscher1',
+                        default='test_monodomain_torord_sb301',
                         help='Directory di output (simID)')
     return parser
 
 def jobID(args):
     return args.outdir
 
-@tools.carpexecution
+@tools.carpexample(parser, jobID)
 def run(args, job):
     # ==========================================
     # 1. Lettura dei nodi e dei tempi di attivazione
     # ==========================================
     print(f"Leggendo i nodi da {args.vtx_file} e i tempi da {args.times_file}...")
-    nodes = np.loadtxt(args.vtx_file, dtype=int)
+    nodes = np.loadtxt(args.vtx_file, dtype=int, skiprows=2)  
     
     # Assicuriamoci che nodes sia iterabile anche in caso contenga un solo nodo
     if nodes.ndim == 0:
@@ -48,7 +52,7 @@ def run(args, job):
     # ==========================================
     # 2. Generazione dei file .vtx per gli stimoli
     # ==========================================
-    vtx_dir = os.path.join(args.outdir, 'activation_vtx')
+    vtx_dir = os.path.join(args.outdir, 'sb301_rootnodes_vtx')
     os.makedirs(vtx_dir, exist_ok=True)
     
     stim_cmds = ['-num_stim', len(nodes)]
@@ -85,8 +89,16 @@ def run(args, job):
         '-bidomain', 0,
         '-parab_solve', 1,
         '-mass_lumping', 1,
-        '-phie_rec_ptf', 'electrode_location_test',
-        '-phie_recovery_file', 'phie_recovery_test'
+        '-phie_rec_ptf', 'sb301_electrodes_opencarp',
+        '-phie_recovery_file', 'sb301_phie_recovery_test'
+    ]
+
+    #caricamento modello torord esterno compilato
+    cmd += [
+        '-num_external_imp', 3,
+        '-external_imp[0]',  os.path.abspath(TORORD_LIB_ENDO),
+        '-external_imp[1]',  os.path.abspath(TORORD_LIB_MID),
+        '-external_imp[2]',  os.path.abspath(TORORD_LIB_EPI),
     ]
 
     # Proprietà di Conduzione (GRegions)
@@ -102,27 +114,43 @@ def run(args, job):
         '-gregion[0].g_in', 0.019,
     ]
 
+    torord_params_common = (
+        'nao=140.0,'
+        'cao=1.8,'
+        'ko=5.4,'
+        'GNa=75.0,'
+        'GNaL=0.0279,'
+        'GKr=0.046,'
+        'GKs=0.006,'
+        'GK1=0.1908,'
+        'Gncx=0.0008,'
+        'GKb=0.003,'
+        'GpCa=0.0005,'
+        'PCa=0.0001007,'
+        'Pnak=30.0'
+    )
+
     # Eterogeneità Cellulare (ImpRegions per il modello tenTusscherPanfilov)
     cmd += [
         '-num_imp_regions', 3,
         
         '-imp_region[0].name', 'Endocardio',
         '-imp_region[0].num_IDs', 1,
-        '-imp_region[0].ID[0]', 1,
-        '-imp_region[0].im', 'tenTusscherPanfilov',
-        '-imp_region[0].im_param', 'flags=ENDO',
+        '-imp_region[0].ID[0]', 3,      #tag endocardio 3 
+        '-imp_region[0].im', 'ToRORd_fkatp_endo',
+        #'-imp_region[0].im_param', torord_params_common,
         
         '-imp_region[1].name', 'Mid_Miocardio',
         '-imp_region[1].num_IDs', 1,
-        '-imp_region[1].ID[0]', 2,
-        '-imp_region[1].im', 'tenTusscherPanfilov',
-        '-imp_region[1].im_param', 'flags=MCELL',
+        '-imp_region[1].ID[0]', 2,      #tag mid miocardio 2
+        '-imp_region[1].im', 'ToRORd_fkatp_endo_mid',
+        #'-imp_region[1].im_param', torord_params_common,
         
         '-imp_region[2].name', 'Epicardio',
         '-imp_region[2].num_IDs', 1,
-        '-imp_region[2].ID[0]', 3,
-        '-imp_region[2].im', 'tenTusscherPanfilov',
-        '-imp_region[2].im_param', 'flags=EPI',
+        '-imp_region[2].ID[0]', 1,      #tag epicardio 1
+        '-imp_region[2].im', 'ToRORd_fkatp_epi',
+        #'-imp_region[2].im_param', torord_params_common,
     ]
 
     # Phys Regions (Dominio Intracellulare Globale)
@@ -143,5 +171,4 @@ def run(args, job):
     job.carp(cmd)
 
 if __name__ == '__main__':
-    args = parser().parse_args()
-    tools.run(args, run)
+    run()

033f6763df92f32748ad2021f39ff86ae2142baf
fedeomb <155232271+FedeOmb@users.noreply.github.com>
2026-06-10 12:46:25 +0000

    fix vari script generate purkinje network

--- opencarp-simulation-ECG/test_generate_purkinje_network_v3.py
+++ opencarp-simulation-ECG/test_generate_purkinje_network_v3.py
@@ -32,7 +32,7 @@ def select_physiological_root_nodes(geometry, all_candidates, lv_candidates, rv_
         [0.7, 0.20]   # 7. RV Free Wall Posterior
     ]
     
-    ab_field = geometry.get_node_vc_field('ab_cut')
+    ab_field = geometry.get_node_vc_field('ab')
     rt_field = geometry.get_node_vc_field('rt')
     
     selected_nodes = []
@@ -116,7 +116,7 @@ def generate_purkinje_network(subject_name, geometric_data_dir, resolution, lv_i
     verbose = True
     # 1. Carica la Geometria
     # Coordinate Cobiveco calcolate
-    vc_name_list = ['ab_cut', 'rt']
+    vc_name_list = ['ab', 'rt']
     #vc_name_list = ['ab', 'rt']    
     resting_vm_value = 0.
     upstroke_vm_value = 1.
@@ -190,7 +190,7 @@ def generate_purkinje_network(subject_name, geometric_data_dir, resolution, lv_i
    # active_root_nodes = all_candidate_root_nodes_index #attivazione di tutti i root nodes
   #  lv_purkinje_edge = lv_candidate_purkinje_edge
   #  rv_purkinje_edge = rv__candidate_purkinje_edge
-    purkinje_speed = 0.2 #cm/ms
+    purkinje_speed = 0.3 #cm/ms
     activation_times_candidates = geometry.get_candidate_root_node_time(purkinje_speed=purkinje_speed)
     # 5. Esportazione per openCARP (.vtx) - candidate root nodes
     vtx_filename = os.path.join(output_dir, f"{subject_name}_candidate_root_nodes.vtx")
@@ -198,7 +198,7 @@ def generate_purkinje_network(subject_name, geometric_data_dir, resolution, lv_i
     times_filename = os.path.join(output_dir, f"{subject_name}_candidate_root_nodes_times.csv")
     np.savetxt(times_filename, activation_times_candidates, fmt='%.4f', header="activation_time_ms", comments='')
     print(f"Generati {len(all_candidate_root_nodes_index)} root nodes candidati e salvati per opencarp in {vtx_filename}")
-
+    
     # 6. Seleziona root nodes fisiologici basati sulle coordinate Cobiveco
     if num_candidate_root_nodes == 0:
         print("Nessun root node candidato trovato.")
@@ -225,12 +225,15 @@ def generate_purkinje_network(subject_name, geometric_data_dir, resolution, lv_i
     sel_times_filename = os.path.join(output_dir, f"{subject_name}_selected_root_nodes_times.csv")
     np.savetxt(sel_times_filename, activation_times_selected, fmt='%.4f', header="activation_time_ms", comments='')
     print(f"Selezionati {len(active_root_nodes)} root nodes casuali e salvati in {sel_vtx_filename}")
-
+    write_root_node_csv(filename=subject_name + '_' + resolution + '_selected_root_nodes_coords.csv', node_vc_list=node_vc_list, node_xyz=node_xyz,
+                    root_node_index_list=active_root_nodes, vc_name_list=vc_name_list, verbose=verbose,
+                    visualisation_dir=output_dir, xyz_name_list=get_xyz_name_list())
     return geometry, active_root_nodes, all_candidate_root_nodes_index, activation_times_selected, activation_times_candidates
 
 
 def map_purkinje_to_fine(subject_name, geometric_data_dir, coarse_resolution, fine_resolution, 
-                         coarse_geometry, active_root_nodes, candidate_root_nodes, activation_times_selected, activation_times_candidates, coarse_units="cm", fine_units="um"):
+                         coarse_geometry, active_root_nodes, candidate_root_nodes, activation_times_selected, activation_times_candidates, 
+                         coarse_units="cm", fine_units="um", output_dir_name="purkinje"):
     print(f"Mappatura della rete di Purkinje da {coarse_resolution} a {fine_resolution}...")
     
     # 1. Carica i nodi della mesh fine
@@ -267,17 +270,10 @@ def map_purkinje_to_fine(subject_name, geometric_data_dir, coarse_resolution, fi
     active_root_nodes_fine = coarse_to_fine_mapping[active_root_nodes]
     candidate_root_nodes_fine = coarse_to_fine_mapping[candidate_root_nodes]
     # 5. Salva i risultati
-    output_dir = os.path.join(geometric_data_dir, subject_name, f"{subject_name}_{fine_resolution}", "purkinje/")
+    output_dir = os.path.join(geometric_data_dir, subject_name, f"{subject_name}_{fine_resolution}", output_dir_name)
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
-        
-    # Se la mesh fine non era in micrometri, salva una versione extra in um per visualizzazione con openCARP
-   # if scale_factor > 1e-4:
-   #     print("Generazione file VTK extra in micrometri per compatibilità visualizzazione openCARP...")
-   #     nodes_to_write_um = fine_node_xyz * (1e-4 / scale_factor) # Converte in um
-   #     write_purkinje_vtk(edge_list=lv_pk_edge_fine, filename=subject_name + '_candidate_LV_Purkinje_mapped_um', node_xyz=nodes_to_write_um, verbose=True, visualisation_dir=output_dir)
-   #     write_purkinje_vtk(edge_list=rv_pk_edge_fine, filename=subject_name + '_candidate_RV_Purkinje_mapped_um', node_xyz=nodes_to_write_um, verbose=True, visualisation_dir=output_dir)    
-    
+
     vtx_filename = os.path.join(output_dir, f"{subject_name}_{fine_resolution}_active_root_nodes.vtx")
     np.savetxt(vtx_filename, active_root_nodes_fine, fmt='%d')
     print(f"Salvati {len(active_root_nodes_fine)} root nodes mappati in {vtx_filename}")
@@ -308,18 +304,18 @@ if __name__ == "__main__":
     data_dir = path_dict["data_path"]
     geometric_data_dir = data_dir + 'geometric_data/'
 
-    subject_name = 'sb4101'
-    output_dir = 'purkinje'
+    subject_name = 'sb3101'
+    output_dir = 'purkinje300'
     coarse_resolution = 'coarse1500cm'
     fine_resolution = 'fine500um'
 
 
     geometry, active_root_nodes, candidate_root_nodes, activation_times_selected, activation_times_candidates = generate_purkinje_network(
                                                     subject_name=subject_name, geometric_data_dir=geometric_data_dir, resolution=coarse_resolution, 
-                                                    lv_inter_node_dist=2.5, rv_inter_node_dist=2.5, output_dir_name=output_dir_name)
+                                                    lv_inter_node_dist=2.5, rv_inter_node_dist=2.5, output_dir_name=output_dir)
     
     # Mappatura su fine
     if os.path.exists(os.path.join(geometric_data_dir, subject_name, f"{subject_name}_{fine_resolution}")):
         map_purkinje_to_fine(subject_name, geometric_data_dir, coarse_resolution, fine_resolution, geometry, 
                              active_root_nodes, candidate_root_nodes, activation_times_selected, activation_times_candidates, 
-                             coarse_units="cm", fine_units="um")
+                             coarse_units="cm", fine_units="um", output_dir_name=output_dir)
